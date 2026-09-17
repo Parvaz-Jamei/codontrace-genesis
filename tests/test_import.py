8a06623b085ee79638208de4af57602533da90bc
Parvaz Jamei <75752108+Parvaz-Jamei@users.noreply.github.com>
2026-09-17 14:39:58 +0400

    test: accept 0.3.0b4.dev1 development identity

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,7 +4,7 @@ import codontrace
 
 
 def test_import_and_version() -> None:
-    assert codontrace.__version__ in {"0.3.0b4", "0.3.0b4.dev0"}
+    assert codontrace.__version__ in {"0.3.0b4", "0.3.0b4.dev0", "0.3.0b4.dev1"}
     assert codontrace.SemanticGenome is codontrace.Genome
     assert codontrace.WhiteBoxAgent is codontrace.Agent
     assert codontrace.ATPAccount is codontrace.ATPBudget
